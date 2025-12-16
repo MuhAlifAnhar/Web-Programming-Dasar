a8dd3124998eba1d1c684dc827c5cad545943ae2
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-16 21:45:20 +0800

    fix

---
+++ DasarDasarJavaScript/app.js
@@ -0,0 +1,11 @@
+let nilai = 50;
+
+if (nilai >= 80 ) {
+    console.log('Dapat A');
+} else if (nilai >= 70) {
+    console.log('Dapat B');
+} else if (nilai >= 60) {
+    console.log('Dapat C');
+} else {
+    console.log('Error')
+}
