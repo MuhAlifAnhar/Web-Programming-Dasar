1239a9472eb8067127df01201d64d28aa8db9c31
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-25 12:50:53 +0800

    fix

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,24 +1,3 @@
-const kampus = [
-    {
-        nama : 'Universitas Negeri Makassar',
-        singkatan : 'UNM'
-    },
-    {
-        nama : 'Universitas Hasanuddin',
-        singkatan : 'UNHAS'
-    },
-    {
-        nama : 'Universitas Dipa Makassar',
-        singkatan : 'UNDIPA'
-    }
-];
-
-kampus[0].singkatan = 'UNEM';
-
-kampus[3].nama = 'Universitas Musim Indonesia';
-
-kampus[3].singkatan = 'UMI';
-
-console.log(kampus[3].singkatan);
-
-console.log(kampus[0].singkatan);
+for (let i = 0; i<=20; i+=2) {
+    console.log(i);
+}
