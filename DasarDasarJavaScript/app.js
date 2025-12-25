532ea4f68f9020098c364eccfaf3d7da92edf78d
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-25 09:13:37 +0800

    fix

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -2,5 +2,12 @@ let student = ['alif', 'anhar', 'muh'];
 let guru = ['alif', 'anhar', 'muh'];
 
 const sekolah = student.concat(guru);
+sekolah.sort();
 
 console.log(sekolah);
+
+let nilai = [5000000, 2, 3000, 400];
+
+nilai.sort();
+
+console.log(nilai);
