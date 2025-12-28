cb4d465e45521043df95de32edf75e33219d65fa
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-28 16:44:00 +0800

    function bernilai balik function

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,12 +1,15 @@
-function duaKali(func) {
-    func();
-    func();
-    func();
+function hasilnyaAdalahFunction() {
+    const rand = Math.random();
+    if(rand > 0.10){
+        return function() {
+            console.log('Selamat, angkanya lebih besar');
+        }
+    } else {
+        return function() {
+            console.log('Maaf, mungkin bisa coba lagi');
+        };
+    }
 }
 
-function lemparDadu() {
-    const nilai = Math.floor(Math.random() * 6) + 1;
-    console.log(nilai);
-}
-
-duaKali(lemparDadu);
+const result = hasilnyaAdalahFunction();
+result();
