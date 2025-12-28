710cd3f791311207dbeec0ce6eb3bb21a3b1aa38
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-28 16:50:54 +0800

    definisi sebuah method

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,15 +1,13 @@
-function hasilnyaAdalahFunction() {
-    const rand = Math.random();
-    if(rand > 0.10){
-        return function() {
-            console.log('Selamat, angkanya lebih besar');
-        }
-    } else {
-        return function() {
-            console.log('Maaf, mungkin bisa coba lagi');
-        };
+const aritmatika = {
+    perkalian: function (x, y) {
+        return x * y
+    },
+    pembagi: function (x, y) {
+        return x / y
+    },
+    pangkat: function (x){
+        return x* x
     }
 }
 
-const result = hasilnyaAdalahFunction();
-result();
+console.log(aritmatika.pembagi(2, 2));
