e531c5dd571aac4a04463725f1ec189a16d38254
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-31 07:45:39 +0800

    arrow function

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,23 +1,5 @@
-const animes = [
-    {
-        name: 'Naruto',
-        genre: 'Shounen',
-        episodes: 700
-    },
-    {
-        name: 'One Piece',
-        genre: 'Shounen',
-        episodes: 1000
-    },
-    {
-        name: 'Bleach',
-        genre: 'Shounen',
-        episodes: 500
-    }
-];
+const perpangkatan = (x) => {
+    return x * x;
+}
 
-const animeList = animes.map(function(anime){
-    return anime.name
-})
-
-console.log(animeList);
+console.log(perpangkatan(5));
