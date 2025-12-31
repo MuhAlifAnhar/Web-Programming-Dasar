c6eec119829e86cc3ad75bc19db523db744d7ac7
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-31 08:44:58 +0800

    filter

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,4 +1,24 @@
-const interval = setInterval(() => {
-    console.log(Math.floor(Math.random() * 100));
-}, 2000);
+const animes = [
+    {
+        name: 'Naruto',
+        episode: 700
+    },
+    {
+        name: 'One Piece',
+        episode: 1100
+    },
+    {
+        name: 'Bleach',
+        episode: 500
+    }
+];
 
+const anime = animes.filter((anime) => {
+    return anime.episode > 500
+})
+
+const anim = anime.map((a) => {
+    return a.name
+})
+
+console.log(anim);
