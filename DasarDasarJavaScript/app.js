0980833fd528cd844dadb2b5b63f738323e1db0a
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-31 16:50:12 +0800

    sunboxing versi array dan object

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,10 +1,8 @@
 const nama = ['Alif', 'Azizi', 'Putra', 'Anhar', 'Rahman', 'Maulana', 'Muh.'];
 
-const pemenang = (gold, silver, bronze, ...sisa) => {
-    console.log(`Mendali emas diraih: ${gold}`);
-    console.log(`Mendali perak diraih: ${silver}`);
-    console.log(`Mendali perunggu diraih: ${bronze}`);
-    console.log(`Peserta lainnya: ${sisa}`);
-}
+const [gold, silver, bronze, ...peserta] = nama;
 
-console.log(pemenang(...nama));
+console.log(gold);
+console.log(silver);
+console.log(bronze);
+console.log(peserta);
