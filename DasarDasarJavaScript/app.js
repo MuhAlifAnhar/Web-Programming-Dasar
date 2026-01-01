aad208941b472312f6f3d52faa54eee44089c84a
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-01 21:01:38 +0800

    fix

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,42 +1,37 @@
-function convertColour (r,g,b) {
-    const color = {};
-
-    color.r = r;
-    color.g = g;
-    color.b = b;
-
-    color.rgb = function () {
-        return `rgb(${this.r}, ${this.g}, ${this.b})`;
+class Color {
+    constructor(r,g,b,name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
     }
 
-    return color;
+    innerRGB() {
+        const { r,g,b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    colorName() {
+        console.log('The color is ' + this.name);
+    }
+
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+
+    hex() {
+        const { r,g,b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+
+    rgba(a = 1.0) {
+        const { r,g,b } = this;
+        return `rgba(${r}, ${g}, ${b}, ${a})`;
+    }
 }
 
-function Color (r,g,b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-};
+const skyColor = new Color(185, 243, 252, 'sky blue');
 
-Color.prototype.rgb = function () {
-    const { r,g,b } = this;
-    return `rgb(${r}, ${g}, ${b})`;
-};
+console.log(skyColor.hex());
 
-Color.prototype.hex = function () {
-    const { r,g,b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-
-Color.prototype.rgba = function (a = 1.0) {
-    const { r,g,b } = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
-
-console.log(Color(255,0,0));
-
-console.log(new Color(255,0,0).rgb());
-
-const objColor = new Color(255,0,0);
-
-console.log(document.body.style.backgroundColor = new Color(19,200,30).rgba());
+console.log(document.body.style.backgroundColor = skyColor.rgb());
