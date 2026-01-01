0aeda0a59dad3ee1c44ebd89d355069e08357da4
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-01 08:28:14 +0800

    oop

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,24 +1,29 @@
-const animes = [
-    {
-        name: 'Naruto',
-        genre: 'Shonen'
-    },
-    {
-        name: 'One Piece',
-        genre: 'Shonen'
-    },
-    {
-        name: 'Bleach',
-        genre: 'Shonen'
-    },
-    {
-        name: 'Naruto Shippuden',
-        genre: 'Shonen'
+const hex = (r, g, b) => {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+const rgb = (r,g,b) => {
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+function convertColour (r,g,b) {
+    const color = {};
+
+    color.r = r;
+    color.g = g;
+    color.b = b;
+
+    color.rgb = function () {
+        return `rgb(${this.r}, ${this.g}, ${this.b})`;
     }
-]
 
-const anime = animes.map(({name, genre}) => {
-    return `${name} ${genre}`;
-})
+    return color;
+}
 
-console.log(anime);
+const color = convertColour(106,22,90);
+console.log(color.rgb());
+
+console.log(convertColour(106,22,90));
+
+console.log(hex(106,22,90));
+console.log(rgb(106,22,90));
