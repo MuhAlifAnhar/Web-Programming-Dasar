3df456f224b3aa3853b2052dd05f71e7003ca97c
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-02 21:05:59 +0800

    oop

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -4,6 +4,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
     }
 
     innerRGB() {
@@ -28,10 +29,64 @@ class Color {
         const { r,g,b } = this;
         return `rgba(${r}, ${g}, ${b}, ${a})`;
     }
+
+    hsl() {
+        const { h,s,l } = this;
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
+
+    fullySaturated() {
+        const { h,l } = this;
+        return `hsl(${h}, 100%, ${l}%)`;
+    }
+
+    opposite() {
+        const { h,s,l } = this;
+        const newHue = (h + 180) % 360;
+        return `hsl(${newHue}, ${s}%, ${l}%)`;
+    }
+
+    calcHSL() {
+        let { r,g,b } = this;
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        let cmin = Math.min(r,g,b),
+            cmax = Math.max(r,g,b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+
+        if(delta === 0) h=0;
+        else if(cmax == r) h = ((g-b) / delta) % 6;
+        else if(cmax == g) h = (b-r) / delta + 2;
+        else h = (r-g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        if(h<0) h += 360;
+
+        l = (cmax+cmin)/2;
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2*l-1));
+
+        s = +(s*100).toFixed(1);
+        l = +(l*100).toFixed(1);
+
+        this.h = h;
+        this.s = s;
+        this.l = l;
+    }
 }
 
 const skyColor = new Color(185, 243, 252, 'sky blue');
 
+
 console.log(skyColor.hex());
 
-console.log(document.body.style.backgroundColor = skyColor.rgb());
+console.log(document.body.style.backgroundColor = skyColor.hsl());
+
+console.log(document.body.style.backgroundColor = skyColor.fullySaturated());
+
+console.log(document.body.style.backgroundColor = skyColor.opposite());
