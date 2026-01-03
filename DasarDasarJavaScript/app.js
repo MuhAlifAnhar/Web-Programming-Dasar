b4d6de9c0e9f8a799305928b112e539bd5f96057
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-03 16:58:39 +0800

    promise

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,33 +1,9 @@
-// const contohPromise = () => {
-//     return new Promise ((resolve, reject) => {
-//     // resolve(() => {
-//     //     console.log('berhasil')
-//     // });
-//     reject(() => {
-//         'gagal';
-//     })
-// })
-// }
-
-// const req = contohPromise();
-
-// console.log(req);
-
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve() ;
-        }, delay);
-    });
+const hello = async () => {
+    throw 'maaf g kenal';
 }
 
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('black', 1000))
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('black', 1000));
+hello().then((res) => {
+    console.log('response', res);
+}).catch((error) => {
+    console.log('error', error);
+})
