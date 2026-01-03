85f83d9d4b6efd299bdb9db567fe90c806ecf10c
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-03 08:36:56 +0800

    asncy

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,38 +1,9 @@
-class Peliharaan {
-    constructor(nama, age) {
-        this.nama = nama;
-        this.age = age;
-    }
+const perkalian = (x,y) => x*y;
 
-    makan() {
-        return `${this.nama} sedang makan`;
-    }
+const akar = (x) => perkalian(x,x);
+
+const pythagoras = (a,b,c) => {
+    return `${akar(a)} + ${akar(b)} === ${akar(c)}`
 }
 
-class Kucing extends Peliharaan {
-    constructor(nama, age, nyawa) {
-        super(nama, age);
-        this.nyawa = nyawa;
-    }
-    meong() {
-        return `meong meong meong`;
-    }
-}
-
-class Ajing extends Peliharaan {
-    gong() {
-        return `gong gong gong`;
-    }
-}
-
-const kucing = new Kucing ('Meong', 2);
-
-const anjing = new Ajing ('Anjing', 3);
-
-console.log(kucing.makan());
-
-console.log(anjing.makan());
-
-console.log(kucing.meong());
-
-console.log(anjing.gong());
+console.log(pythagoras(3,4,5));
