3034f8383545a6fc5bf80243624197a998ac1934
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-03 13:36:56 +0800

    sync

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,20 +1,23 @@
-setTimeout(function () {
-    document.body.style.backgroundColor = 'red';
-        setTimeout(function () {
-            document.body.style.backgroundColor = 'yellow';
-                setTimeout(function () {
-                    document.body.style.backgroundColor = 'green';
-                        setTimeout(function () {
-                            document.body.style.backgroundColor = 'blue';
-                        }, 1000);
-                }, 1000);
-        }, 1000);
-}, 1000);
+const requestCallback = (url, success, failure) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+        if (delay > 4000) {
+            failure('Error: Connection Timeout');
+        } else {
+            success(`Success: ${url} (${delay}ms)`);
+        }
+    }, delay)
+}
 
-// setTimeout(function () {
-//     document.body.style.backgroundColor = 'red';
-// }, 2000);
-
-// setTimeout(function () {
-//     document.body.style.backgroundColor = 'yellow';
-// }, 3000);
+const requestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * 4500) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Error: Connection Timeout');
+            } else {
+                resolve(`Success: ${url} (${delay}ms)`);
+            }
+        }, delay);
+    });
+};
