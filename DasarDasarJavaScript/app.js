0f8153a4913c00009d86072aa7fe92d16d8813d7
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-03 17:18:57 +0800

    promise

--- DasarDasarJavaScript/app.js
+++ DasarDasarJavaScript/app.js
@@ -1,9 +1,26 @@
-const hello = async () => {
-    throw 'maaf g kenal';
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay);
+    })
 }
 
-hello().then((res) => {
-    console.log('response', res);
-}).catch((error) => {
-    console.log('error', error);
-})
+async function changeColor() {
+    await delayedColorChange('red', 1000);
+    await delayedColorChange('orange', 1000);
+    await delayedColorChange('yellow', 1000);
+    await delayedColorChange('green', 1000);
+    await delayedColorChange('blue', 1000);
+    await delayedColorChange('indigo', 1000);
+    await delayedColorChange('violet', 1000);
+    return 'All done!';
+}
+
+async function printRainbow() {
+    await changeColor();
+    console.log('done');
+}
+
+printRainbow();
